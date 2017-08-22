f99851bdb2bea649a1cfc6cffb63bc4e92d3ff76
Genitivo <samp.92@hotmail.it>
2017-08-22 16:42:43 +0200

    cluster creation for completed order

--- sessions_analysis.py
+++ sessions_analysis.py
@@ -5,12 +5,28 @@ def potentialOrderSession(sessions):
 
 def completedOrderSession(sessions):
 	return filter(lambda x: True if 'L' in x.split(',')[0] else False,sessions)
-#
-# def createCluster(s):
-
 
 neighbors = []
 total_cluster = []
+
+def createCluster(s):
+	add = False
+	new_total_cluster = []
+
+	if len(total_cluster) > 0:
+		for cluster in total_cluster:
+			if any(s[0].split(',')[0] in st for st in cluster) and add == False:
+				temp = list(set(cluster + s[1:len(s)]))
+				new_total_cluster.append(temp)
+				add = True
+			else:
+				new_total_cluster.append(cluster)
+
+	if add == False:
+		new_total_cluster.append(s)
+
+	total_cluster[:] = new_total_cluster
+
 
 with open("neighbors.csv", 'r') as file:
 	reader = csv.reader(file, delimiter='\t')
@@ -26,4 +42,6 @@ potential_completed_orders = map(potentialOrderSession, completedOrder_neighbors
 # SESSIONI CON ACQUISTO E VICINI
 completed_order_sessions = map(completedOrderSession, completedOrder_neighbors)
 
-print completed_order_sessions
+map(createCluster,completed_order_sessions)
+
+print total_cluster
