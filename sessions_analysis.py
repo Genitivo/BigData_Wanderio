2ac5aea4575132a20e5ec4a8b91b95dfa1e1e7c9
CostanzaBrachetti <costanza.brachetti@gmail.com>
2017-09-06 19:14:57 +0200

    cluster potenziali acquisti

--- sessions_analysis.py
+++ sessions_analysis.py
@@ -1,4 +1,6 @@
 import csv
+import re 
+import functools 
 
 def potentialOrderSession(sessions):
 	return filter(lambda x: True if 'L' not in x.split(',')[0] else False,sessions)
@@ -27,24 +29,25 @@ def findDensity(cluster):
 
 
 
-def createCluster(s):
+def createCluster(s,order_sessions):
 	new_total_cluster = []
 	capoCat = s[0].split(',')[0]
 	minValCC = 10
 	candidateCluster = []
 
-	for cluster in completed_order_sessions_copy:
+	for cluster in order_sessions:
 		if capoCat!=cluster[0].split(',')[0]:
 			for st in cluster:
-				if (capoCat == st.split(',')[0]) and (float(st.split(',')[1]) <= minValCC):
-					minValCC = st.split(',')[1]
-					candidateCluster = cluster
+				if len(st.split(','))>1:
+					if (capoCat == st.split(',')[0]) and (float(st.split(',')[1]) <= minValCC):
+						minValCC = st.split(',')[1]
+						candidateCluster = cluster
 
 
-	if candidateCluster in completed_order_sessions_copy:
-		completed_order_sessions_copy.remove(candidateCluster)
-	if s in completed_order_sessions_copy:
-		completed_order_sessions_copy.remove(s)
+	if candidateCluster in order_sessions:
+		order_sessions.remove(candidateCluster)
+	if s in order_sessions:
+		order_sessions.remove(s)
 
 	temp = removeDuplicateWithMinScore(s+candidateCluster)
 
@@ -53,7 +56,7 @@ def createCluster(s):
 
 	total_cluster.append(temp)
 	# print total_cluster
-	completed_order_sessions_copy.append(temp)
+	order_sessions.append(temp)
 
 
 def pickOnlyZero(cluster):
@@ -94,40 +97,76 @@ def removeDuplicati(cluster):
 
 	return removeDuplicateWithMinScore(cluster)
 
+
+
+def pulisciLista(pot):
+	return pot[0:len(pot)-1]	
+
+
+	
+
+def createClusterPotOrder(pot):
+	cap = pot[0].split(',')[0]
+	capNeighbors = newNeighbors.get(cap)
+	newPot = [cap]
+
+	for p in pot[1:len(pot)-1]:
+		a = re.escape(p.split(',')[0])
+		regex = re.compile(a+',.*')
+		newP = [m.group(0) for l in capNeighbors for m in [regex.search(l)] if m]
+		if newP:
+			newPot.append(newP[0])
+	return newPot
+
+	
+
+
 with open("neighbors.csv", 'r') as file:
 	reader = csv.reader(file, delimiter='\t')
 	neighbors.append(list(reader))
 
+
 neighbors = neighbors[0]
-
-
-
 
 completedOrder_neighbors = filter(lambda x: True if 'L' in x[0].split(',')[0] else False, neighbors)
 
+newNeighbors = map(lambda lista: (lista[0].split(',')[0],lista[1:len(lista)-1]),neighbors)
+newNeighbors = dict(newNeighbors)
+
+
+
 # SESSIONI CON POTENZIALE ACQUISTO
 potential_completed_orders = map(potentialOrderSession, completedOrder_neighbors)
+potential_completed_orders = map(pulisciLista, potential_completed_orders)
+potential_completed_orders = filter(lambda x: True if x else False,potential_completed_orders)
+
+pco_cluster = map(createClusterPotOrder, potential_completed_orders)
+pco_cluster_copy = pco_cluster[:]
+
+print len(pco_cluster)
+map(functools.partial(createCluster,order_sessions=pco_cluster_copy),pco_cluster)
+
+print len(pco_cluster)
+
+
 
 # SESSIONI CON ACQUISTO E VICINI
 completed_order_sessions = map(completedOrderSession, completedOrder_neighbors)
-
 completed_order_sessions_copy = completed_order_sessions[:]
 
-map(createCluster,completed_order_sessions)
-filteredList = map(pickOnlyZero,total_cluster)
+#map(createCluster,completed_order_sessions)
+#filteredList = map(pickOnlyZero,total_cluster)
 # result = map(removeDuplicati,completed_order_sessions)
 
-listWithDensity = map(findDensity,filteredList)
-#
-print listWithDensity
+#listWithDensity = map(findDensity,filteredList)
 
 
-neighbors_file =  open('./clusterDensity.csv', "a")
-for elem in listWithDensity:
-	for e in elem[0]:
-		neighbors_file.write("%s\t" % e)
-    	neighbors_file.write("%s\n" % elem[1])
-neighbors_file.close()
 
-# print len(total_cluster)
-# print '*********************************'
+# neighbors_file =  open('./clusterDensity.csv', "a")
+# for elem in listWithDensity:
+# 	neighbors_file.write("%s\t" % elem[1])
+# 	for e in elem[0]:
+# 		neighbors_file.write("%s\t" % e)
+# 	neighbors_file.write("\n")
+# neighbors_file.close()
+
